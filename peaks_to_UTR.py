e24afa0439bacd79b67bae189df89d79e3f21bd9
Will Haese-Hill <William.Haese-Hill@glasgow.ac.uk>
2021-11-15 14:27:38 +0000

    forward and reverse strand compatibility

--- peaks_to_UTR.py
+++ peaks_to_UTR.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import collections
 import csv
+from dataclasses import dataclass
 
 import gffutils
 
@@ -10,63 +10,91 @@ maxDistanceFromTranscript = 200
 gff_in = "Orig.PRFA01000011.gff"
 gff_out = gff_in.split('.')[0] + '.db'
 
-forward_broad_peaks = "forward_peaks.broadPeak"
-reverse_broad_peaks = "reverse_peaks.broadPeak"
+FORWARD_PEAKS_FILENAME = "forward_peaks.broadPeak"
+REVERSE_PEAKS_FILENAME = "reverse_peaks.broadPeak"
 
 db = gffutils.create_db(gff_in, gff_out, force=True)
 
-Peak = collections.namedtuple("Peak", 'chr start end name score strand signalValue pValue qValue')
+
+@dataclass
+class Peak:
+    chr: str
+    start: int
+    end: int
+    name: str
+    score: int
+    strand: str
+    signalValue: float
+    pValue: float
+    qValue: float
+
+    def __post_init__(self):
+        # Enforce typing
+        for k, v in self.__annotations__.items():
+            self.__setattr__(k, v(self.__getattribute__(k)))
+
 
 out = ''
 
-strands = [(forward_broad_peaks, '+'), (reverse_broad_peaks, '-')]
+strands = [(FORWARD_PEAKS_FILENAME, '+'), (REVERSE_PEAKS_FILENAME, '-')]
 
-# TODO adapt for reverse strand as well
-f = open(forward_broad_peaks, "r")
-broad_peaks = csv.reader(f, delimiter="\t")
-for peak in broad_peaks: 
-    peak = Peak(*peak)
-    features = list(db.region(
-        seqid=peak.chr,
-        start=int(peak.start) - maxDistanceFromTranscript,
-        end=int(peak.end) + maxDistanceFromTranscript,
-        strand='+')
-    )
-    if features:
-        if any([f for f in features if f.featuretype == 'three_prime_UTR']):
-            print("3' UTR already annotated for features near peak %s" % peak.name)
-            continue
-        genes = [f for f in features if f.featuretype == 'gene']
-        for idx, gene in enumerate(genes):
-            if gene.start < int(peak.start) and gene.end > int(peak.end):
-                print("Peak %s wholly contained within gene %s" % (peak.name, gene.id))
-                continue
-            if len(genes) > idx + 1:
-                if int(peak.start) < gene.end and int(peak.end) > genes[idx + 1].start:
-                    print("Peak %s overlapping gene %s and gene %s" % (peak.name, gene.id, genes[idx + 1].id))
+for peaks_filename, strand in strands:
+    with open(peaks_filename, 'r') as f:
+        peaks = csv.reader(f, delimiter="\t")
+        for peak in peaks:
+            peak = Peak(*peak)
+            peak_range = range(peak.start, peak.end)
+            features = list(db.region(
+                seqid=peak.chr,
+                start=peak.start - maxDistanceFromTranscript,
+                end=peak.end + maxDistanceFromTranscript,
+                strand=strand)
+            )
+            if features:
+                if any([f for f in features if f.featuretype == 'three_prime_UTR']):  # Same for both strands
+                    print("3' UTR already annotated for features near peak %s" % peak.name)
                     continue
-            if int(peak.end) > gene.end:
-                print("PEAK %s CORRESPONDS TO 3' UTR OF GENE %s" % (peak.name, gene.id))
-                print("-----> UTR = (%s, %s)" % (gene.end, peak.end))
-                attrs = dict(gene.attributes)
-                attrs['ID'] = [gene.id + "_UTR"]
-                attrs['Parent'] = [gene.id]
-                attrs['colour'] = ['3']
-                f = gffutils.Feature(
-                    seqid=gene.chrom,
-                    source="3pUTR_annotation",
-                    featuretype="three_prime_UTR",
-                    start=gene.end,
-                    end=peak.end,
-                    score='.',
-                    strand=gene.strand,
-                    frame='.',
-                    attributes=attrs
-                )
-                out += str(f) + '\n'
+                genes = [f for f in features if f.featuretype == 'gene']
+                genes = list(reversed(genes)) if strand == '-' else genes
+                for idx, gene in enumerate(genes):
+                    gene_range = range(gene.start, gene.end)
+                    if set(peak_range).issubset(gene_range):
+                        print("Peak %s wholly contained within gene %s" % (peak.name, gene.id))
+                        continue
+                    if len(genes) > idx + 1:
+                        next_gene = genes[idx + 1]
+                        next_gene_range = range(next_gene.start, next_gene.end)
+                        if set(peak_range).intersection(gene_range) and set(peak_range).intersection(next_gene_range):
+                            print("Peak %s overlapping gene %s and gene %s" % (peak.name, gene.id, next_gene.id))
+                            continue
+                    if strand == "+" and peak.end > gene.end:
+                        utr = {'start': gene.end, 'end': peak.end}
+                    elif strand == "-" and peak.start < gene.start:
+                        utr = {'start': peak.start, 'end': gene.start}
+                    else:
+                        utr = {}
+                    if utr:
+                        print("PEAK %s CORRESPONDS TO 3' UTR OF GENE %s" % (peak.name, gene.id))
+                        print("-----> UTR = ({}, {})".format(utr['start'], utr['end']))
+                        attrs = dict(gene.attributes)
+                        attrs['ID'] = [gene.id + "_UTR"]
+                        attrs['Parent'] = [gene.id]
+                        attrs['colour'] = ['3']
+                        f = gffutils.Feature(
+                            seqid=gene.chrom,
+                            source="3pUTR_annotation",
+                            featuretype="three_prime_UTR",
+                            start=utr['start'],
+                            end=utr['end'],
+                            score='.',
+                            strand=gene.strand,
+                            frame='.',
+                            attributes=attrs
+                        )
+                        out += str(f) + '\n'
 
-    else:
-        print("No features found near peak %s" % peak.name)
+            else:
+                print("No features found near peak %s" % peak.name)
 
 with open('three_prime_UTRs.gff', 'w') as fout:
     fout.write(out)
