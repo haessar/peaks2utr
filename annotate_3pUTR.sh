f82aeac6aadc0394b5e9bea214101a6754658790
Will Haese-Hill <William.Haese-Hill@glasgow.ac.uk>
2021-11-30 09:41:10 +0000

    first iteration of usage with docopts

--- annotate_3pUTR.sh
+++ annotate_3pUTR.sh
@@ -1,3 +1,28 @@
+#!/usr/bin/env bash
+#
+# cat -n all files
+#
+# Usage: cat-n_wrapper_example.sh [--count=N] FILE...
+#
+# Arguments:
+#   FILE     input file, if FILLE equal - stdin is used instead.
+#
+# Options:
+#   --count=N   limit the number of line to display
+#
+# Examples:
+#    ./cat-n_wrapper.sh --count=3 cat-n_wrapper.sh  quick_example.sh
+#
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+export PATH=$parent_path:$PATH
+
+source docopts.sh
+help=$(docopt_get_help_string $0)
+version='0.1'
+
+parsed=$(docopts -G args -h "$help" -V $version : "$@")
+eval "$parsed"
+
 GFF_IN="$2"
 
 PYTHON_CODE=$(cat <<END
@@ -14,13 +39,13 @@ END
 
 GFF_DB="$(python3 -c "$PYTHON_CODE")"
 
-../../call_forward_peaks.sh "$1" &
-../../call_reverse_peaks.sh "$1" &
+call_forward_peaks.sh "$1" &
+call_reverse_peaks.sh "$1" &
 
 wait
 
-../../peaks_to_3pUTR/peaks_to_UTR.py "$GFF_DB" forward_peaks.broadPeak forward --max-distance "$3" &
-../../peaks_to_3pUTR/peaks_to_UTR.py "$GFF_DB" reverse_peaks.broadPeak reverse --max-distance "$3" &
+peaks_to_UTR.py "$GFF_DB" forward_peaks.broadPeak forward --max-distance "$3" &
+peaks_to_UTR.py "$GFF_DB" reverse_peaks.broadPeak reverse --max-distance "$3" &
 
 wait
 
