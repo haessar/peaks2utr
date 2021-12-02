89dd1e4131ab35bf2142a8ab8e88210f67477ee6
Will Haese-Hill <wahhill@gmail.com>
2021-12-02 16:04:53 +0000

    delete now redundant shell scripts

--- call_reverse_peaks.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f "$1.reverse.bam" ]; then
-  samtools view --threads 6 -b -f 16 "$1.bam" > "$1.reverse.bam"
-fi
-
-if [ ! -f reverse_peaks.broadPeak ]; then
-  macs2 callpeak -t "$1.reverse.bam" -n reverse --nomodel --extsize 100 --broad
-fi
-
