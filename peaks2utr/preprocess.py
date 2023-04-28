2588d407af4e4735e7dbc55a8210daa808e084c6
Will Haese-Hill <wahhill@gmail.com>
2023-04-28 22:36:48 +0100

    fix to enable inferring of gene/transcript features from gtf input

--- peaks2utr/preprocess.py
+++ peaks2utr/preprocess.py
@@ -156,7 +156,7 @@ async def create_db(gff_in):
     if not os.path.isfile(gff_db):
         logging.info('Creating gff db.')
         await sync_to_async(gffutils.create_db)(
-            gff_in, gff_db, force=True, verbose=True, disable_infer_genes=True, disable_infer_transcripts=True)
+            gff_in, gff_db, force=True, verbose=True)
         logging.info('Finished creating gff db.')
     else:
         logging.info("Using cached gff db.")
