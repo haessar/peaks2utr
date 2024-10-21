439d8a73d7b7becfe941f69a1efde416476ff159
William Haese-Hill <wahhill@gmail.com>
2024-10-21 16:59:30 +0100

    fix breaking tests

--- tests/no_strand_overlap/test_no_strand_overlap.py
+++ tests/no_strand_overlap/test_no_strand_overlap.py
@@ -25,14 +25,12 @@ class TestNoStrandOverlap(unittest.TestCase):
         reverse_peaks_filename = os.path.join(TEST_DIR, "reverse_peaks.broadPeak")
         self.reverse_peaks = BroadPeaksList(broadpeak_fn=reverse_peaks_filename, strand="reverse")
         self.argparser = prepare_argparser()
-        self.args = self.argparser.parse_args(["", ""])
-        self.args.max_distance = 2000
 
     def tearDown(self):
         os.remove(os.path.join(TEST_DIR, "Chr1.db"))
 
-    def _do_annotation(self, expected_annotations):
-        pipeline = AnnotationsPipeline(self.forward_peaks + self.reverse_peaks, self.args, queue=Queue())
+    def _do_annotation(self, args, expected_annotations):
+        pipeline = AnnotationsPipeline(self.forward_peaks + self.reverse_peaks, args, queue=Queue())
         for peak in self.forward_peaks + self.reverse_peaks:
             if peak.name in expected_annotations:
                 pipeline.annotate_utr_for_peak(self.db, peak, self.truncation_points, self.coverage_gaps)
@@ -47,32 +45,30 @@ class TestNoStrandOverlap(unittest.TestCase):
                     self.assertEqual(annotations.data[gene]['utr'].range, expected_annotations[peak.name][gene].range)
 
     def test_utr_no_overlap(self):
-        self.args.no_strand_overlap = True
+        args = self.argparser.parse_args(["", "", "--max-distance", "2000", "--no-strand-overlap"])
         expected_annotations = {
             "forward_peak_104": {"C4B63_1g127": UTR(278110, 280146)},
             "reverse_peak_99": {"C4B63_1g1319": UTR(279445, 280146)}
         }
-        self._do_annotation(expected_annotations)
+        self._do_annotation(args, expected_annotations)
     
     def test_utr_no_overlap_do_pseudo(self):
-        self.args = self.argparser.parse_args(["", "", "--do-pseudo"])
-        self.args.max_distance = 2000
-        self.args.no_strand_overlap = True
+        args = self.argparser.parse_args(["", "", "--max-distance", "2000", "--no-strand-overlap", "--do-pseudo"])
         expected_annotations = {
             "forward_peak_104": {"C4B63_1g127": UTR(278110, 280028)},
             "reverse_peak_99": {
                 "C4B63_1g1319": UTR(279445, 280146),
                 "C4B63_1g1320": UTR(279445, 280028)}
         }
-        self._do_annotation(expected_annotations)
+        self._do_annotation(args, expected_annotations)
     
     def test_utr_with_overlap(self):
-        self.args.no_strand_overlap = False
+        args = self.argparser.parse_args(["", "", "--max-distance", "2000"])
         expected_annotations = {
             "forward_peak_104": {"C4B63_1g127": UTR(278110, 281990)},
             "reverse_peak_99": {"C4B63_1g1319": UTR(279445, 280146)}
         }
-        self._do_annotation(expected_annotations)
+        self._do_annotation(args, expected_annotations)
 
 if __name__ == '__main__':
     unittest.main()
